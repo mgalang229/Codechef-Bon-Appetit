9e87aee01befe5cae50799b81a09329ee4e04da0
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-02-14 20:53:39 +0800

    Create author-sol.cpp

---
+++ author-sol.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n, k;
+		cin >> n >> k;
+		map<int, vector<pair<int, int>>> mp;
+		for (int i = 0; i < n; i++) {
+			int arrival, departure, compartment;
+			cin >> arrival >> departure >> compartment;
+			mp[compartment].emplace_back(departure, arrival);
+		}
+		int cnt = 0;
+		for (auto& x : mp) {
+			if (x.second.empty()) {
+				continue;
+			}
+			// if we sort this type of container, then it will compare the first value (departure)
+			// however, if the first value is equal, then it will compare the second value (arrival)
+			sort(x.second.begin(), x.second.end());
+			int last = 0;
+			for (pair<int, int> p : x.second) {
+				// if the arrival value of the current customer is greater than or equal to the most recent
+				// departure value of the previous customer, then we increment the counter
+				if (p.second >= last) {
+					cnt++;
+					last = p.first;
+				}
+			}
+		}
+		cout << cnt << '\n';
+ 	}
+	return 0;
+}
